b789e434e10d958d8cc90bbf32cbee3839b1d0f4
Manas.Arora@cginfinity.com <Manas.Arora@cginfinity.com>
2026-07-27 02:57:03 +0530

    fixed api url issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://aquabilling-api-manas-buc9bmdwa2hghaap.centralindia-01.azurewebsites.net/api",
 });
 
 export default api;
